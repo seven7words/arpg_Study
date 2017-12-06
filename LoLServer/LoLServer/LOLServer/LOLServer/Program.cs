a1caf4a175d8cfc0ae0bd9220a4df397ab74717b
woopqww111 <woopqww111@yahoo.com>
2017-12-06 22:08:48 +0800

    no message

---
+++ LoLServer/LoLServer/LOLServer/LOLServer/Program.cs
@@ -0,0 +1,19 @@
+﻿using NetFrame;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LOLServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
